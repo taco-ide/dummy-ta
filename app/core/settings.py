ab6324d97384fa91b942c3e27ccbb7d92a3da6dc
ro-anderson <didier.rda@gmail.com>
2025-01-12 10:29:24 -0300

    chore: Update default Qwen model

--- app/core/settings.py
+++ app/core/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
 
     # HuggingFace Model Configurations
-    MODEL_REPO_ID: str = environ.get("MODEL_REPO_ID", "Qwen/Qwen2-0.5B-Instruct-GGUF")
+    MODEL_REPO_ID: str = environ.get("MODEL_REPO_ID", "Qwen/Qwen2.5-Coder-3B-Instruct-GGUF")
     MODEL_FILENAME: str = environ.get("MODEL_FILENAME", "*q8_0.gguf")
     MODEL_GPU_LAYERS: int = int(environ.get("MODEL_GPU_LAYERS", -1)) # Use all available GPU layers.
     MODEL_CONTEXT_WINDOW: int = int(environ.get("MODEL_CONTEXT_WINDOW", 2048))  # Default context window size
